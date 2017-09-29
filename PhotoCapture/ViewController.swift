0a9488beb5616c63fed1918f709731ced5050123
shotaioramashvili <shotaioramashvili@gmail.com>
2017-09-29 13:48:26 +0400

    initial commit

--- PhotoCapture/ViewController.swift
+++ PhotoCapture/ViewController.swift
@@ -1,25 +1,174 @@
-//
-//  ViewController.swift
-//  PhotoCapture
-//
-//  Created by Shota Ioramashvili on 9/29/17.
-//  Copyright © 2017 Self. All rights reserved.
-//
-
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var previewView: UIView!
+    @IBOutlet weak var captureButton: UIButton!
+    let flashView = UIView()
+    
+    var session = AVCaptureSession()
+    var photoOutput = AVCapturePhotoOutput()
+    var videoPreviewLayer: AVCaptureVideoPreviewLayer!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        setupFlashView()
+        
+        setupSession()
+        setupVideoPreviewLayer()
+        setupPhotoOutput()
+        setupCameraAndStartSession()
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        captureButton.layer.borderColor = UIColor.red.cgColor
+        captureButton.layer.cornerRadius = captureButton.bounds.width / 2
+        captureButton.clipsToBounds = true
     }
-
-
+    
+    @IBAction func captureImageTapped(_ sender: UIButton) {
+        captureButton.isEnabled = false
+        
+        let settings = AVCapturePhotoSettings()
+        photoOutput.capturePhoto(with: settings, delegate: self)
+    }
+    
+    fileprivate func setupSession() {
+        session.sessionPreset = AVCaptureSession.Preset.photo
+    }
+    
+    fileprivate func setupPhotoOutput() {
+        photoOutput.setPreparedPhotoSettingsArray([AVCapturePhotoSettings(format: [AVVideoCodecKey: AVVideoCodecJPEG])], completionHandler: nil)
+    }
+    
+    fileprivate func setupVideoPreviewLayer() {
+        videoPreviewLayer = AVCaptureVideoPreviewLayer(session: session)
+        videoPreviewLayer.videoGravity = AVLayerVideoGravity.resizeAspectFill
+        videoPreviewLayer.connection?.videoOrientation = AVCaptureVideoOrientation.portrait
+        videoPreviewLayer.frame = UIScreen.main.bounds
+        
+        previewView.layer.insertSublayer(videoPreviewLayer, at: 0)
+    }
+    
+    fileprivate func setupCameraAndStartSession() {
+        let backCamera = AVCaptureDevice.default(for: .video)!
+        
+        var input: AVCaptureDeviceInput?
+        do {
+            input = try AVCaptureDeviceInput(device: backCamera)
+        } catch {
+            print(error.localizedDescription)
+        }
+        
+        if let input = input, session.canAddInput(input) {
+            session.addInput(input)
+            
+            if session.canAddOutput(photoOutput) {
+                session.addOutput(photoOutput)
+                
+                session.startRunning()
+            }
+        }
+    }
+    
+    fileprivate func setupFlashView() {
+        flashView.translatesAutoresizingMaskIntoConstraints = false
+        flashView.alpha = 0
+        flashView.backgroundColor = .black
+        previewView.addSubview(flashView)
+        previewView.sendSubview(toBack: captureButton)
+        
+        flashView.topAnchor.constraint(equalTo: previewView.topAnchor).isActive = true
+        flashView.bottomAnchor.constraint(equalTo: previewView.bottomAnchor).isActive = true
+        flashView.leftAnchor.constraint(equalTo: previewView.leftAnchor).isActive = true
+        flashView.rightAnchor.constraint(equalTo: previewView.rightAnchor).isActive = true
+    }
+    
+    fileprivate func animateFlashView() {
+        UIView.animate(withDuration: 0.1) {
+            self.flashView.alpha = 1
+        }
+        
+        UIView.animate(withDuration: 0.3, delay: 0.1, options: .curveEaseInOut, animations: {
+            self.flashView.alpha = 0
+        }, completion: nil)
+    }
+    
+    fileprivate func previewCapturedImage(data: Data?) {
+        if let imageData = data {
+            let image = UIImage(data: imageData)
+            
+            let imageView = UIImageView(image: image)
+            imageView.contentMode = .scaleAspectFill
+            imageView.clipsToBounds = true
+            imageView.frame = CGRect(origin: CGPoint(x: -200, y: 50), size: CGSize(width: 100, height: 150))
+            imageView.alpha = 0
+            
+            view.addSubview(imageView)
+            
+            UIView.animate(withDuration: 0.4, animations: {
+                imageView.frame.origin = CGPoint(x: 20, y: 50)
+                imageView.alpha = 1
+            })
+            
+            UIView.animate(withDuration: 0.6, delay: 3, options: .curveEaseInOut, animations: {
+                imageView.frame.origin.x -= 200
+                imageView.alpha = 0
+            }, completion: { _ in
+                imageView.removeFromSuperview()
+            })
+        }
+    }
 }
+
+extension ViewController: AVCapturePhotoCaptureDelegate {
+    func photoOutput(_ output: AVCapturePhotoOutput, willBeginCaptureFor resolvedSettings: AVCaptureResolvedPhotoSettings) {
+        
+    }
+    
+    func photoOutput(_ output: AVCapturePhotoOutput, willCapturePhotoFor resolvedSettings: AVCaptureResolvedPhotoSettings) {
+        
+    }
+    
+    func photoOutput(_ output: AVCapturePhotoOutput, didFinishProcessingPhoto photoSampleBuffer: CMSampleBuffer?, previewPhoto previewPhotoSampleBuffer: CMSampleBuffer?, resolvedSettings: AVCaptureResolvedPhotoSettings, bracketSettings: AVCaptureBracketedStillImageSettings?, error: Error?) {
+        
+        defer {
+            captureButton.isEnabled = true
+        }
+        
+        animateFlashView()
+        
+        if
+            let sampleBuffer = photoSampleBuffer,
+            let dataImage = AVCapturePhotoOutput.jpegPhotoDataRepresentation(forJPEGSampleBuffer: sampleBuffer, previewPhotoSampleBuffer: previewPhotoSampleBuffer) {
+            
+            previewCapturedImage(data: dataImage)
+        }
+        
+    }
+    
+    @available(iOS 11.0, *)
+    func photoOutput(_ output: AVCapturePhotoOutput, didFinishProcessingPhoto photo: AVCapturePhoto, error: Error?) {
+        defer {
+            captureButton.isEnabled = true
+        }
+        
+        animateFlashView()
+        
+        previewCapturedImage(data: photo.fileDataRepresentation())
+        //        AudioServicesPlayAlertSound(1108)
+    }
+}
+
+
+
+
+
+
+
+
 
