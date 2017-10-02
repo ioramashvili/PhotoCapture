49aa41202e60a0439e1d48beffece36f368b48fd
shotaioramashvili <shotaioramashvili@gmail.com>
2017-10-02 16:15:21 +0400

    add toImage property

---
+++ PhotoCapture/UIView+Extension.swift
@@ -0,0 +1,10 @@
+import UIKit
+
+extension UIView {
+    func toImage() -> UIImage? {
+        let renderer = UIGraphicsImageRenderer(bounds: bounds)
+        return renderer.image { rendererContext in
+            layer.render(in: rendererContext.cgContext)
+        }
+    }
+}
