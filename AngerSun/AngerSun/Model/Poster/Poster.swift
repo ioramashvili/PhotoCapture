561376fc3e5351412ae6eadc46d0e0af07061d47
shotaioramashvili <shotaioramashvili@gmail.com>
2017-10-30 10:43:55 +0400

    minor changes

--- AngerSun/AngerSun/Model/Poster/Poster.swift
+++ AngerSun/AngerSun/Model/Poster/Poster.swift
@@ -25,6 +25,7 @@ class Poster: PosterDataProvider {
     
     static func staticPosters() -> [PosterDataProvider] {
         let posters: [PosterDataProvider] = [
+            MonochromePoster(posterImage: #imageLiteral(resourceName: "04"), intensity: 0.35, color: UIColor(name: "ff1515FF")),
             MonochromePoster(posterImage: #imageLiteral(resourceName: "f1"), intensity: 0.35, color: UIColor(name: "ff1515FF")),
             MonochromePoster(posterImage: #imageLiteral(resourceName: "f2"), intensity: 0.35, color: UIColor(name: "ff1515FF")),
             MonochromePoster(posterImage: #imageLiteral(resourceName: "f3"), intensity: 0.35, color: UIColor(name: "ff1515FF"))
